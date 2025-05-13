3d2ffc9c482dcb9d668fe48acfaf219bd954671c
NguyenHuuVanh <vietbui673@gmail.com>
2025-05-13 14:47:39 +0700

    fixed: package file

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL, // Use environment variable for base URL
+  // baseURL: "http://localhost:3000/api",
   withCredentials: true, // This is important for sending cookies with requests
 });
 
