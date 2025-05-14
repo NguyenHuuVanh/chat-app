2b98a4109ef4c0e1e1b5f3a795557a17313c0eee
NguyenHuuVanh <vietbui673@gmail.com>
2025-05-15 00:03:03 +0700

    fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,18 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
 
-import { connectDB } from "./lib/db.js";
-
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use(
   cors({
@@ -18,10 +21,6 @@ app.use(
     credentials: true,
   })
 );
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.status(200).send("API is running...");
