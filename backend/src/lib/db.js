c4730954c1164e3f4e730406efb7c58f45be230f
NguyenHuuVanh <vietbui673@gmail.com>
2025-05-14 21:27:22 +0700

    fixed: db

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,32 +1,11 @@
 import mongoose from "mongoose";
 
-// export const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGODB_URI);
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error.message);
-//     process.exit(1); // Exit the process with failure
-//   }
-// };
-
-let cached = global.mongoose;
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
 export const connectDB = async () => {
-  if (cached.conn) {
-    return cached.conn;
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); // Exit the process with failure
   }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGODB_URI).then((mongoose) => {
-      console.log(`MongoDB Connected: ${mongoose.connection.host}`);
-      return mongoose;
-    });
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
 };
