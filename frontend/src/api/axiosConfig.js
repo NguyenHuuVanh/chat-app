90bc4f3146910e20ac432714bc7617a11cf4f793
NguyenHuuVanh <vietbui673@gmail.com>
2025-05-15 08:08:51 +0700

    updated: axiosConfig

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const BASEURL =
+  import.meta.env.MODE === "production" ? "https://chat-app-server-alpha-six.vercel.app" : "http://localhost:5001";
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // Use the environment variable for the base URL
+  // baseURL: import.meta.env.VITE_API_URL, // Use the environment variable for the base URL
+  baseURL: BASEURL, // Use the environment variable for the base URL
   withCredentials: true, // This is important for sending cookies with requests
 });
 
