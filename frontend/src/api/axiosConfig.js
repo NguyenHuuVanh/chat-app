adab752b104ad4205622258f67a3b9be7ba8f78f
NguyenHuuVanh <vietbui673@gmail.com>
2025-05-15 17:30:49 +0700

    updated: api

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const BASEURL =
-  import.meta.env.MODE === "production"
-    ? "https://chat-app-server-alpha-six.vercel.app/api"
-    : "http://localhost:5001/api";
+  import.meta.env.MODE === "production" ? "https://chat-app-1-6dz2.onrender.com/api" : "http://localhost:5001/api";
 
 const axiosInstance = axios.create({
   // baseURL: import.meta.env.VITE_API_URL, // Use the environment variable for the base URL
