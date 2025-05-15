039be5ffe147c1ef81eb74e4a833d270c857b002
NguyenHuuVanh <vietbui673@gmail.com>
2025-05-15 23:04:33 +0700

    updated

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const BASEURL =
-  import.meta.env.MODE === "production" ? `${import.meta.env.VITE_API_URL}` : "https://chat-app-1-6dz2.onrender.com";
+  import.meta.env.MODE === "production"
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : "https://chat-app-1-6dz2.onrender.com/api";
 
 const axiosInstance = axios.create({
   baseURL: BASEURL, // Use the environment variable for the base URL
