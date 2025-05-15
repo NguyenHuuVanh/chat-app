6f0722a0f0ef38426207c6bea81a75ef89a6915b
NguyenHuuVanh <vietbui673@gmail.com>
2025-05-15 20:00:59 +0700

    updated

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
-// const BASEURL =
-//   import.meta.env.MODE === "production" ? "https://chat-app-1-6dz2.onrender.com/api" : "http://localhost:5001/api";
+const BASEURL =
+  import.meta.env.MODE === "production"
+    ? `${import.meta.env.VITE_API_URL}`
+    : "https://chat-app-1-6dz2.onrender.com/api";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // Use the environment variable for the base URL
+  baseURL: BASEURL, // Use the environment variable for the base URL
   withCredentials: true, // This is important for sending cookies with requests
 });
 
