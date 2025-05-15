fcf2eb9e7ea81f6ec235b0c6de0b670295c3e9de
NguyenHuuVanh <vietbui673@gmail.com>
2025-05-15 17:07:56 +0700

    updated: cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,6 +20,7 @@ app.use(
     origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
