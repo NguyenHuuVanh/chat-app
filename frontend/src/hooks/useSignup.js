1c23fac16725cd5d15485029a46fc71d8d8f43f8
NguyenHuuVanh <vietbui673@gmail.com>
2025-05-29 22:10:29 +0700

    changes

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,10 +1,23 @@
-import { QueryClient, useMutation } from "@tanstack/react-query";
+import { QueryClient, useMutation, useQueryClient } from "@tanstack/react-query";
 import { signup } from "../api/dataService";
+import { useNavigate } from "react-router-dom";
 
 const useSignup = () => {
+  const navigate = useNavigate();
+  const QueryClient = useQueryClient();
+
   const { mutate, isPending, error } = useMutation({
     mutationFn: signup,
-    onSuccess: () => QueryClient.invalidateQueries({ queryKey: ["authUser"] }),
+    onSuccess: (data) => {
+      console.log("Signup successful:", data);
+      QueryClient.invalidateQueries({ queryKey: ["authUser"] });
+
+      if (data.user.isOnboarded) {
+        navigate("/");
+      } else {
+        navigate("/onboarding");
+      }
+    },
     onError: () => {
       console.error("Error signing up:", error);
     },
